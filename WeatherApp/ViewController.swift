7b20bbce806c8adf57df601d8bce885c61e92045
otetLopez <otet.tud@gmail.com>
2020-01-17 17:38:15 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by otet_tud on 1/17/20.
+//  Copyright © 2020 otet_tud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
